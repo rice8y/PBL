17981ef41f2ffb8e371d258a838c063d84156096
YONEYAMA Eito @ pblone <j520496z@mails.cc.ehime-u.ac.jp>
2024-07-01 00:40:00 +0900

    update

--- pages/logout_form.php
+++ pages/logout_form.php
@@ -1,21 +1,30 @@
-<?php  session_start() ?>
+<?php session_start() ?>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>logout form</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
+
 <body>
-    <section class="vh-100" style="background-color: white;">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
+    <section class="vh-100 d-flex align-items-center" style="background-color: white;">
+        <div class="container">
+            <div class="row justify-content-center">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div class="card shadow-2-strong" style="border-radius: 1rem;">
                         <div class="card-body p-5 text-center">
                             <h3 class="mb-5">ログアウト</h3>
                             <form action="login.php" method="POST" id="logout-form" novalidate>
-                                <button data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-lg btn-block" type="submit" name="login">ログアウト</button>
+                                <div class="mb-3">
+                                    <button class="btn btn-primary btn-lg btn-block" type="submit" name="login">ログアウト</button>
+                                </div>
+                                <div>
+                                    <button class="btn btn-secondary btn-lg btn-block" type="button" name="back"
+                                        onclick="javascript:history.back()">戻る</button>
+                                </div>
                             </form>
                         </div>
                     </div>
@@ -23,6 +32,9 @@
             </div>
         </div>
     </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
