66b6f427191bca5631fb089d399df028a485104e
YONEYAMA Eito @ pblone <j520496z@mails.cc.ehime-u.ac.jp>
2024-07-01 09:17:29 +0900

    update

--- pages/home.php
+++ pages/home.php
@@ -71,7 +71,7 @@ if (!isset($_SESSION['username'])) {
             </div>
         </div>
     </div>
-    <script>
+    <!-- <script>
         const myDiv =document.getElementById('plot');
         var data = [
             {
@@ -83,7 +83,7 @@ if (!isset($_SESSION['username'])) {
 
         Plotly.newPlot(myDiv, data);
 
-    </script>
+    </script> -->
     <!-- contents -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
