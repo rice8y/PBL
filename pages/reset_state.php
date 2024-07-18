c492c687cf8da6aa133ff5616ade9f28b9036ec1
YONEYAMA Eito @ pblone <j520496z@mails.cc.ehime-u.ac.jp>
2024-07-19 02:20:00 +0900

    update

--- pages/reset_state.php
+++ pages/reset_state.php
@@ -1,10 +1,13 @@
 <?php
 try {
-    $db_file = "sqlite3.db";
-    $sqlite = new SQLite3($db_file);
+    $db_file = "/home/j496yone/public_html/pblone/test/sqlite3.db";
+    $tbl = "check_lists";
+    $sqlite = new SQLite3($db_file, SQLITE3_OPEN_READWRITE);
+    $sqlite->enableExceptions(true);
 
-    $query = "UPDATE check_lists SET state = 0";
-    $sqlite->exec($query);
+    $stmt = $sqlite->prepare("UPDATE $tbl SET state = :state");
+    $stmt->bindValue(':state', 0, SQLITE3_INTEGER);
+    $result = $stmt->execute();
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage() . "\n";
 }
