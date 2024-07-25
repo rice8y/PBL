6241caa3c33a91ce9254fbd0a10abbef1fbf1ec9
YONEYAMA Eito @ pblone <j520496z@mails.cc.ehime-u.ac.jp>
2024-07-25 14:47:25 +0900

    update

--- init.sh
+++ init.sh
@@ -14,3 +14,13 @@ CURRENT_DIR=$(pwd)
 echo "0 0 * * * /usr/bin/php $CURRENT_DIR/pages/reset_state.php" >> "$TEMP_CRON"
 crontab "$TEMP_CRON"
 rm "$TEMP_CRON"
+
+SERVER_NAME=$(hostname)
+PUBLICK_LINK="https://$SERVER_NAME/$CURRENT_DIR"
+
+if [ $? -eq 0 ]; then
+    echo "Successfully initislized."
+    echo "Please access the following link: $PUBLICK_LINK"
+else
+    echo "Error: Initialization failed."
+fi
